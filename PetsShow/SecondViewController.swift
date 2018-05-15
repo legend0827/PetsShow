9bd51ce72623e0b3d9b58c676ce3287a8abee1e6
Kevin <dqwen@doulian.com>
2018-05-15 09:02:17 +0800

    first Commit

---
+++ PetsShow/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PetsShow
+//
+//  Created by Kevin on 2018/5/14.
+//  Copyright © 2018 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
